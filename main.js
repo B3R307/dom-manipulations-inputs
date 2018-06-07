4f79a5ae5dcf85bfb43602063e84b8851bcd82aa
B3R307 <talzolteotlmeztli@gmail.com>
2018-06-06 21:25:47 -0500

    exercise four done

--- main.js
+++ main.js
@@ -1,54 +1,94 @@
 document.querySelector("#reset-field button").addEventListener('click',function(){
   // TASK #1
-   var pushEl = document.querySelector('button') //selecciono el elemento
+    var pushEl = document.querySelector('button') //selecciono el elemento
     var pushElDom = pushEl.value
 
-   var userInputDomEl = document.querySelector('input') //selecciono el elemento
+    var userInputDomEl = document.querySelector('input') //selecciono el elemento
    // console.log(userInputDomEl);
-   var inputEl = userInputDomEl.value  //userInputDomEl recibira la informacion , agregamos la propiedad .value para devolver el valor del atributo que en este caso es lo que escribe el usuario, esto se guarda en una nueva variable
+    var inputEl = userInputDomEl.value  //userInputDomEl recibira la informacion , agregamos la propiedad .value para devolver el valor del atributo que en este caso es lo que escribe el usuario, esto se guarda en una nueva variable
    // console.log(inputEl);
-   inputEl.innerHTML = pushElDom
+      inputEl.innerHTML = pushElDom
 
-   userInputDomEl.value = ''
+      userInputDomEl.value = ''
 
 })
 
-document.querySelector("#validate-field button").addEventListener('click',function changeMessager(someEvt){
+document.querySelector("#validate-field button").addEventListener('click',function(){
   // TASK #2
-var pushButt = document.querySelector('button')
-var pussButtEl =pushButt.value
-// console.log(pushButt);
-var inputBoxText = document.querySelector('input')
-var boxTextEl = someEvt.target
-// console.log(someEvt.target);
-var inputVal = boxTextEl.value
-// console.log(inputVal);
-var validFlash = document.querySelector('.flash-message')
+  var pushButt = document.querySelector('#validate-field button')
+
+  var inputBoxText = document.querySelector('#validate-field input')
+  var inputBoxEl = inputBoxText.value
+  var inputEl = inputBoxEl.length
+  // console.log(inputEl);
+  var flashOutputEl = document.querySelector('#validate-field .flash-message')
 // console.log(validFlash);
-var messValidFlash = validFlash.value
+// inputBoxText.innerHTML = inputEl.length
 
-console.log(inputVal.length);
-inputBoxText.innerHTML = inputVal.length
-
-// if(inputVal.length < 6){
-//   messValidFlash.className.add('p.flash-message.invalid')
-// }
-// if(boxTextEl.length > 6){
-//   messValidFlash.className.remove('p.flash-message.invalid')
-//   messValidFlash.className.add('p.flash-message.valid')
-// }
-
-
+    if(inputEl < 6){
+      flashOutputEl.classList.remove('valid');
+      flashOutputEl.classList.add('invalid');
+      flashOutputEl.textContent = "Input not long enough"
+    } else {
+       flashOutputEl.classList.remove('invalid');
+       flashOutputEl.classList.add('valid');
+       flashOutputEl.textContent = "Field valid"
+    }
 })
 
 
 document.querySelector("#calculate-items button").addEventListener('click',function(){
   // TASK #3
 
+    var elements = [...document.querySelectorAll('#calculate-items input')]
+    // console.log(elements);
+
+    var totalCount = document.querySelector('.sum-total')
+    // console.log(totalCount);
+
+    var total = 0
+
+    for(var i = 0; i < elements.length; i++){
+      var currentElement = elements[i]
+      // console.log(currentElement);
+      // console.log(currentElement.checked);
+      //
+      if(currentElement.checked === true){
+      total = total + parseInt(currentElement.value)
+
+      }
+    }
+    // console.log(total);
+    totalCount.innerHTML = total
 })
 
 
-document.querySelector("#select-to-show-more button").addEventListener('click',function(){
+document.querySelector("#select-to-show-more select").addEventListener('change',function(evt){
   // TASK #4
+
+    var carsElementDom = document.querySelector('.selection')
+      // console.log(carsElementDom.value)
+      var index = carsElementDom.selectedIndex
+
+     var imgInfo = document.querySelector('.img-box')
+     var modelInfo = document.querySelector('.model');
+     var msrpInfo = document.querySelector('.msrp');
+     var mpgInfo = document.querySelector('.mpg');
+     var edmunds = document.querySelector('.edmunds')
+
+     var selectedIndex = evt.target.options.selectedIndex
+     var indexEl = evt.target.options[selectedIndex]
+
+
+      imgInfo.innerHTML  = `<img src="./images/${indexEl.dataset.img}">`
+      modelInfo.innerHTML = carsElementDom.value
+      msrpInfo.innerHTML =  indexEl.dataset.msrp
+      mpgInfo.innerHTML = indexEl.dataset.mpg
+      edmunds.innerHTML =   indexEl.dataset.edmunds
+
+
+      // console.log(evt.target.value)
+//         console.log(evt.target.options)
+
 
 })
